fe139eb80177eabab86c3e311ff329b76ae56008
Haisam <elkewidyhaisam@gmail.com>
2017-11-08 15:44:39 -0500

    commit anyway

--- Humanity/src/main/java/com/humanity/controllers/PreviewController.java
+++ Humanity/src/main/java/com/humanity/controllers/PreviewController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Set;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
